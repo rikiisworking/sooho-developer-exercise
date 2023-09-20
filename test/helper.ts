9c9d247f550d78d2b40642380444f703be295097
rikiisworking <rikiisworking@gmail.com>
2023-09-21 04:15:55 +0900

    refactor: code formatting

--- test/helper.ts
+++ test/helper.ts
@@ -1,8 +1,10 @@
 import { EventLog, ContractTransactionReceipt } from "ethers";
 
 const getEvent = (receipt: ContractTransactionReceipt, target: string) => {
-  const result = (receipt.logs || []).filter((element) => element instanceof EventLog && element.fragment.name === target);
+  const result = (receipt.logs || []).filter(
+    (element) => element instanceof EventLog && element.fragment.name === target,
+  );
   return result.length ? result[0].args || [] : [];
-}
+};
 
 export { getEvent };
