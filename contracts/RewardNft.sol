2cb2f023c33fe915e4de94cdf4ee1c80c1340bec
Riki <rikkki@tenping.kr>
2023-09-21 01:49:30 +0900

    Feature/reward nft (#2)
    * feat: basic functionalities
    
    * test: update testcode, add gitignores
    
    ---------
    
    Co-authored-by: rikiisworking <rikiisworking@gmail.com>

--- contracts/RewardNft.sol
+++ contracts/RewardNft.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract RewardNft {
-    function mint(address to) public {
-        // implements here
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RewardNft is ERC721, Ownable {
+    uint256 public tokenId = 0;
+
+    constructor() ERC721("You are a VIP", "VIP") Ownable() {}
+
+    function mint(address to) public onlyOwner {
+        _mint(to, tokenId);
+        tokenId += 1;
     }
 }
